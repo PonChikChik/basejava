3c6527bfc8b1af7b5323c0a27f3adabbd06269aa
Aleksandr Agafonov <alex.2exe2@gmail.com>
2021-01-13 20:00:44 +0500

    hw0: create methods for ArrayStorage class: 1: clear - remove all notnull elements from array; 2: save - save element in array after notnull elements; 3: get - search element and show from array; 4: delete - delete element and reindex array; 5: getAll - get all elements from array; 6: size - return array size;

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,83 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; ++i) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            }
+        }
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        for (int i = 0; i < storage.length; ++i) {
+            if (storage[i] == null) {
+                storage[i] = resume;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        for (Resume resume : storage) {
+            if (resume == null) {
+                break;
+            }
+
+            if (resume.uuid.equals(uuid)) {
+                return resume;
+            }
+        }
+
+        Resume emptyResume = new Resume();
+        emptyResume.uuid = "null";
+
+        return emptyResume;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < this.size(); ++i) {
+            Resume resume = storage[i];
+
+            if (resume.uuid.equals(uuid)) {
+                this.storage = this.changeIndexes(storage, i);
+                break;
+            }
+        }
+    }
+
+    Resume[] changeIndexes(Resume[] resumes, int index) {
+        resumes[index] = resumes[index + 1];
+
+        for (int i = index + 1; i < resumes.length; ++i) {
+            if (i == resumes.length - 1) {
+                resumes[i] = null;
+            } else {
+                resumes[i] = resumes[i + 1];
+            }
+
+        }
+
+        return resumes;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return storage;
     }
 
     int size() {
-        return 0;
+        int result = 0;
+
+        for(Resume resume : storage) {
+            if (resume == null) {
+                break;
+            }
+
+            ++result;
+        }
+
+        return result;
     }
 }
