c32e09fde64ec86f9f7349ed3a93728b21b6591b
Aleksandr Agafonov <alex.2exe2@gmail.com>
2021-01-26 23:55:02 +0500

    hw0: code style

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,11 +3,10 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-
     int size = 0;
 
     void clear() {
-        for (int i = 0; i < size; ++i) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
 
@@ -16,14 +15,12 @@ public class ArrayStorage {
 
     void save(Resume resume) {
         storage[size] = resume;
-        ++size;
+        size++;
     }
 
     Resume get(String uuid) {
-        for (Resume resume : storage) {
-            if (resume == null) {
-                break;
-            }
+        for (int i = 0; i < size; i++) {
+            Resume resume = storage[i];
 
             if (resume.uuid.equals(uuid)) {
                 return resume;
@@ -34,30 +31,15 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < size; ++i) {
+        for (int i = 0; i < size; i++) {
             Resume resume = storage[i];
 
             if (resume.uuid.equals(uuid)) {
-                this.storage = this.changeIndexes(storage, i);
-                --size;
+                System.arraycopy(storage, i + 1, storage, i, storage.length - 1 - i);
+                size--;
                 break;
             }
         }
-    }
-
-    Resume[] changeIndexes(Resume[] resumes, int index) {
-        resumes[index] = resumes[index + 1];
-
-        for (int i = index + 1; i < resumes.length; ++i) {
-            if (i == resumes.length - 1) {
-                resumes[i] = null;
-            } else {
-                resumes[i] = resumes[i + 1];
-            }
-
-        }
-
-        return resumes;
     }
 
     /**
@@ -66,9 +48,7 @@ public class ArrayStorage {
     Resume[] getAll() {
         Resume[] result = new Resume[size];
 
-        for (int i = 0; i < size; ++i) {
-            result[i] = storage[i];
-        }
+        if (size >= 0) System.arraycopy(storage, 0, result, 0, size);
 
         return result;
     }
