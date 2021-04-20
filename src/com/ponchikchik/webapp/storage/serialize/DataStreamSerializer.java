ef103037c7e4d584b3e1b08060a77c2e4d8e21b5
Aleksandr Agafonov <alex.2exe2@gmail.com>
2021-04-20 17:25:03 +0500

    hw10: fix errors with saving and reading data from file

--- src/com/ponchikchik/webapp/storage/serialize/DataStreamSerializer.java
+++ src/com/ponchikchik/webapp/storage/serialize/DataStreamSerializer.java
@@ -4,7 +4,7 @@ import com.ponchikchik.webapp.model.*;
 
 import java.io.*;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class DataStreamSerializer implements StreamSerializer {
@@ -15,7 +15,6 @@ public class DataStreamSerializer implements StreamSerializer {
             dataOutputStream.writeUTF(resume.getFullName());
 
             Map<ContactType, String> contacts = resume.getContacts();
-            dataOutputStream.writeInt(contacts.size());
             writeCollection(dataOutputStream, contacts.entrySet(), (entry) -> {
                 dataOutputStream.writeUTF(entry.getKey().name());
                 dataOutputStream.writeUTF(entry.getValue());
@@ -23,8 +22,8 @@ public class DataStreamSerializer implements StreamSerializer {
 
             Map<SectionType, AbstractSection> sections = resume.getSections();
             writeCollection(dataOutputStream, sections.entrySet(), (entry) -> {
-                dataOutputStream.writeUTF(entry.getKey().name());
                 SectionType sectionName = entry.getKey();
+                dataOutputStream.writeUTF(sectionName.name());
                 AbstractSection sectionValue = entry.getValue();
 
                 switch (sectionName) {
@@ -40,26 +39,25 @@ public class DataStreamSerializer implements StreamSerializer {
                     case EXPERIENCE:
                     case EDUCATION:
                         writeCollection(dataOutputStream, ((OrganizationList) sectionValue).getOrganizationList(), (organization) -> {
-                            dataOutputStream.writeUTF(Objects.requireNonNullElse(organization.getCompanyName(), ""));
-                            dataOutputStream.writeUTF(Objects.requireNonNullElse(organization.getWebsite().getName(), ""));
-                            dataOutputStream.writeUTF(Objects.requireNonNullElse(organization.getWebsite().getUrl(), ""));
+                            dataOutputStream.writeUTF(Objects.requireNonNullElse(organization.getWebsite().getName(), "null"));
+                            dataOutputStream.writeUTF(Objects.requireNonNullElse(organization.getWebsite().getUrl(), "null"));
 
                             writeCollection(dataOutputStream, organization.getExperienceList(), (experience) -> {
-                                dataOutputStream.writeUTF(Objects.requireNonNullElse(experience.getTitle(), ""));
-                                dataOutputStream.writeUTF(Objects.requireNonNullElse(experience.getDescription(), ""));
+                                dataOutputStream.writeUTF(Objects.requireNonNullElse(experience.getTitle(), "null"));
+                                dataOutputStream.writeUTF(Objects.requireNonNullElse(experience.getDescription(), "null"));
 
                                 LocalDate startDate = experience.getStartDate();
                                 if (startDate != null) {
                                     dataOutputStream.writeUTF(prepareLocalDate(experience.getStartDate()));
                                 } else {
-                                    dataOutputStream.writeUTF("");
+                                    dataOutputStream.writeUTF(prepareLocalDate(LocalDate.of(0, 1, 1)));
                                 }
 
                                 LocalDate endDate = experience.getEndDate();
                                 if (endDate != null) {
                                     dataOutputStream.writeUTF(prepareLocalDate(experience.getEndDate()));
                                 } else {
-                                    dataOutputStream.writeUTF("");
+                                    dataOutputStream.writeUTF(prepareLocalDate(LocalDate.of(0, 1, 1)));
                                 }
                             });
                         });
@@ -80,9 +78,7 @@ public class DataStreamSerializer implements StreamSerializer {
 
             readCollection(dataInputStream, () -> {
                 String contactTypeName = dataInputStream.readUTF();
-                if (!contactTypeName.equals("")) {
-                    resume.addContact(ContactType.valueOf(contactTypeName), dataInputStream.readUTF());
-                }
+                resume.addContact(ContactType.valueOf(contactTypeName), dataInputStream.readUTF());
             });
             readCollection(dataInputStream, () -> {
                 SectionType sectionType = SectionType.valueOf(dataInputStream.readUTF());
@@ -136,30 +132,44 @@ public class DataStreamSerializer implements StreamSerializer {
             case OBJECTIVE:
                 return new TextSection(dataInputStream.readUTF());
             case ACHIEVEMENT:
-            case EXPERIENCE:
-                return new ListSection(readList(dataInputStream, dataInputStream::readUTF));
             case QUALIFICATION:
+                return new ListSection(readList(dataInputStream, dataInputStream::readUTF));
+            case EXPERIENCE:
             case EDUCATION:
                 return new OrganizationList(
-                        readList(dataInputStream, () -> new Organization(
-                                new Link(dataInputStream.readUTF(), dataInputStream.readUTF()),
-                                readList(dataInputStream, () -> new Organization.Experience(
-                                        dataInputStream.readUTF(),
-                                        dataInputStream.readUTF(),
-                                        revertToLocalDate(dataInputStream.readUTF()),
-                                        revertToLocalDate(dataInputStream.readUTF())
-                                ))
-                        )));
+                        readList(dataInputStream, () -> {
+                            Link homePage = new Link(revertFromFile(dataInputStream.readUTF()), revertFromFile(dataInputStream.readUTF()));
+                            return new Organization(
+                                    homePage,
+                                    readList(dataInputStream, () -> new Organization.Experience(
+                                            revertFromFile(dataInputStream.readUTF()),
+                                            revertFromFile(dataInputStream.readUTF()),
+                                            revertToLocalDate(dataInputStream.readUTF()),
+                                            revertToLocalDate(dataInputStream.readUTF())
+                                    ))
+                            );
+                        }));
             default:
                 throw new Error("Unknown category");
         }
     }
 
     private String prepareLocalDate(LocalDate localDate) {
-        return localDate.format(new DateTimeFormatterBuilder().toFormatter());
+        return localDate.format(DateTimeFormatter.ISO_LOCAL_DATE);
     }
 
     private LocalDate revertToLocalDate(String localDate) {
+        if ("0000-01-01".equals(localDate)) {
+            return null;
+        }
         return LocalDate.parse(localDate);
+    }
+
+    private String revertFromFile(String value) {
+        if (value.equals("null")) {
+            return null;
+        }
+
+        return value;
     }
 }
