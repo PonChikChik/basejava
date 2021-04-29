ee184a4af4bcdf0347802b9be419722a04e28777
Aleksandr Agafonov <alex.2exe2@gmail.com>
2021-04-29 12:35:49 +0500

    hw12: add task on Stream api

--- src/com/ponchikchik/webapp/MainConcurrency.java
+++ src/com/ponchikchik/webapp/MainConcurrency.java
@@ -1,7 +1,9 @@
 package com.ponchikchik.webapp;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
@@ -9,6 +11,8 @@ public class MainConcurrency {
     private static final Object LOCK = new Object();
 
     public static void main(String[] args) throws InterruptedException {
+        System.out.println("First " + minValue(new int[]{3, 5, 1, 6, 6, 6}));
+        System.out.println("Second " + oddOrEven(Arrays.asList(3, 5, 1, 6, 6, 6)));
         System.out.println(Thread.currentThread().getName());
 
         Thread thread0 = new Thread() {
@@ -62,5 +66,18 @@ public class MainConcurrency {
 
     private synchronized void inc() {
         counter++;
+    }
+
+    private static int minValue(int[] values) {
+        return Arrays.stream(values).distinct().sorted().reduce(0, (int acc, int value) -> 10 * acc + value);
+    }
+
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        AtomicInteger sum = new AtomicInteger();
+        Predicate<Integer> evenPredicate = x -> x % 2 == 0;
+        return integers.stream()
+                .peek(sum::addAndGet)
+                .collect(Collectors.partitioningBy(evenPredicate))
+                .get(evenPredicate.negate().test(sum.get()));
     }
 }
